411db6286a635899a60f2196d54c2ca1d8d87ac4
AgohChris <agihchris90@gmail.com>
2025-04-13 12:33:18 +0000

    Auto-commit du : 13-04-2025 à 12:33:18

--- djangoGpt/settings.py
+++ djangoGpt/settings.py
@@ -74,12 +74,12 @@ WSGI_APPLICATION = 'djangoGpt.wsgi.application'
 
 
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'chatminipy@gmail.com'
-EMAIL_HOST_PASSWORD = 'kjfk rzll xkfb bowz'
+EMAIL_BACKEND = os.getenv("EMAIL_BACKEND")
+EMAIL_HOST = os.getenv("EMAIL_HOST")
+EMAIL_PORT = os.getenv("EMAIL_PORT")
+EMAIL_USE_TLS = os.getenv("EMAIL_USE_TLS")
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
 DEFAULT_FROM_MAIL = EMAIL_HOST_USER
 
 
